fb2cbd13b0c21f8f19552a78a47bf08d637203b9
EnumaOub <rayane.oubenali@gmail.com>
2024-08-12 11:56:13 +0200

    test env netlify

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,17 @@ import { md5 } from 'js-md5';
 
 const getMarvel = async (name) => {
   const ts = parseInt(Date.now() / 1000, 10);
-  const key = import.meta.env.VITE_MARVEL_PUBLIC_KEY;
-  const hash = md5(ts+import.meta.env.VITE_MARVEL_PRIVATE_KEY + key)
+  let key;
+  let keyPrivate;
+  if (process.env.NETLIFY === 'true') {
+    key = process.env.VITE_MARVEL_PUBLIC_KEY;
+    keyPrivate = process.env.VITE_MARVEL_PRIVATE_KEY;
+   } else {
+    key = import.meta.env.VITE_MARVEL_PUBLIC_KEY;
+    keyPrivate = import.meta.env.VITE_MARVEL_PRIVATE_KEY;
+   }
+  
+  const hash = md5(ts + keyPrivate + key)
   const url = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${key}&hash=${hash}&nameStartsWith=${name}&limit=1`;
 
   const response = await fetch(url);
