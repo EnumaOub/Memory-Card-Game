b3915c9bfdcd09cb201147279a6441c9416a9cb3
EnumaOub <rayane.oubenali@gmail.com>
2024-08-18 16:16:34 +0200

    Style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import './Header.css'
+import MarvelLogo from '../assets/marvel'
 
 export function Header() {
     return (
         <header>
             <div id="title-page">
-                <h1>Card Game</h1>
+                <MarvelLogo className="marvel-logo"/>
+                <h1> Memory Card Game</h1>
             </div>
             <div id="fetching">
                 <label htmlFor="fetch-bool">Fetching (slower)</label>
-                <input type="checkbox" id='fetch-bool' defaultChecked />
+                <input type="checkbox" id='fetch-bool' />
             </div>
             
         </header>
